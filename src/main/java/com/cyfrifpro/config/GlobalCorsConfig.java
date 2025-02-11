e96cdf91a1a345bd094a1b52dfb41c9a2a53b0f8
BenuKhamari <cyfrifuiprobenudhar@gmail.com>
2025-02-11 15:21:54 +0530

    new ch

--- src/main/java/com/cyfrifpro/config/GlobalCorsConfig.java
+++ src/main/java/com/cyfrifpro/config/GlobalCorsConfig.java
@@ -1,25 +1,25 @@
-package com.cyfrifpro.config;
+// package com.cyfrifpro.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-public class GlobalCorsConfig {
+// @Configuration
+// public class GlobalCorsConfig {
 
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOriginPattern("http://88.222.241.45:2001"); // Allow specific origin
-		// config.addAllowedOriginPattern("http://127.0.0.1:5501"); // Allow specific origin
-		config.addAllowedHeader("*"); // Allow all headers
-		config.addAllowedMethod("*"); // Allow all HTTP methods
-		config.setAllowCredentials(true); // Allow credentials (e.g., cookies, Authorization headers)
+// 	@Bean
+// 	public CorsFilter corsFilter() {
+// 		CorsConfiguration config = new CorsConfiguration();
+// 		config.addAllowedOriginPattern("http://88.222.241.45:2001"); // Allow specific origin
+// 		// config.addAllowedOriginPattern("http://127.0.0.1:5501"); // Allow specific origin
+// 		config.addAllowedHeader("*"); // Allow all headers
+// 		config.addAllowedMethod("*"); // Allow all HTTP methods
+// 		config.setAllowCredentials(true); // Allow credentials (e.g., cookies, Authorization headers)
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config); // Apply to all endpoints
-		return new CorsFilter(source);
-	}
-}
+// 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+// 		source.registerCorsConfiguration("/**", config); // Apply to all endpoints
+// 		return new CorsFilter(source);
+// 	}
+// }
